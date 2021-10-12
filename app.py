39d718e2cae52d9e1688f98520e3cc7b155bd2fc
modshehri <mod.shehri@hotmail.com>
2021-10-12 04:18:30 +0300

    Implementing all routes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
-
+from datetime import date
 from models import db_drop_and_create_all, setup_db, Actor, Movie
 from auth import AuthError, requires_auth
 
@@ -24,8 +24,11 @@ CORS(app)
 db_drop_and_create_all()
 
 # ROUTES
+
+# Actors Routes
+
 @app.route('/actors')
-def get_index():
+def get_actors():
     actors = Actor.query.order_by(Actor.id).all()
     return jsonify({
         "success": True, 
@@ -64,7 +67,7 @@ def patch_actors(actor_id):
         actor.name = data['name']
 
     if 'age' in data:
-        actor.recipe = data['age']
+        actor.age = data['age']
 
     if 'gender' in data:
         actor.gender = data['gender']
@@ -76,6 +79,81 @@ def patch_actors(actor_id):
         "actor": actor.get_json()
     })
 
+@app.route('/actors/<actor_id>', methods=['DELETE'])
+def delete_actors(actor_id):
+    actor = Actor.query.get(actor_id)
+    if actor is None:
+        abort(404)
+
+    actor.delete()
+
+    return jsonify({
+        "success": True, 
+        "delete": actor_id
+    })
+
+
+# Movies Routes 
+@app.route('/movies')
+def get_movies():
+    movies = Movie.query.order_by(Movie.id).all()
+    return jsonify({
+        "success": True, 
+        "movies": [movie.get_json() for movie in movies]
+    })
+
+
+@app.route('/movies', methods=['POST'])
+def create_movies():
+    data = request.get_json()
+
+    if 'title' and 'release_date' not in data:
+        abort(422)
+    
+    movie_release_date = date.fromisoformat(data['release_date'])
+    movie_title = data['title']
+
+    movie = Movie(title=movie_title, release_date=movie_release_date)
+    movie.insert()
+
+    return jsonify({
+        "success": True, 
+        "movie": movie.get_json()
+    })
+
+@app.route('/movies/<movie_id>', methods=['PATCH'])
+def patch_movies(movie_id):
+    movie = Movie.query.get(movie_id)
+    if movie is None:
+        abort(404)
+
+    data = request.get_json()
+    
+    if 'title' in data:
+        movie.name = data['title']
+
+    if 'release_date' in data:
+        movie.release_date = date.fromisoformat(data['release_date'])
+
+    movie.update()
+
+    return jsonify({
+        "success": True, 
+        "movie": movie.get_json()
+    })
+
+@app.route('/movies/<movie_id>', methods=['DELETE'])
+def delete_movies(movie_id):
+    movie = Movie.query.get(movie_id)
+    if movie is None:
+        abort(404)
+
+    movie.delete()
+
+    return jsonify({
+        "success": True, 
+        "delete": movie_id
+    })
 # Error Handling
 '''
 Example error handling for unprocessable entity
@@ -90,22 +168,6 @@ def unprocessable(error):
         "message": "unprocessable"
     }), 422
 
-
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-'''
 @app.errorhandler(404)
 def notFound(error):
     return jsonify({
@@ -114,10 +176,6 @@ def notFound(error):
         "message": "resource not found"
     }), 404
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
 @app.errorhandler(AuthError)
 def authorization_error(error):
     error.error["success"] = False
